600d1c63eb903df899c9b8f4b565869394bf5183
Onyinye Chukwuma <onyinyechukwuma14@gmail.com>
2021-12-29 00:01:35 +0100

    'made-the-mathematical-signs-functional'

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const operations = document.querySelectorAll(".operations");
 const equals = document.querySelector(".equals");
 const reset = document.querySelector(".reset");
 
+let hiddenDisplay = "";
 let inputNumber = "";
 let haveDot = false;
+let result = null;
+let lastOperation = "";
 
 controls.forEach((number) => {
   number.addEventListener("click", (e) => {
@@ -17,4 +20,39 @@ controls.forEach((number) => {
     inputNumber += e.target.textContent;
     input.value = inputNumber;
   });
+});
+operations.forEach((operation) => {
+  operation.addEventListener("click", (e) => {
+    if (!inputNumber) return;
+    haveDot = false;
+    const operationName = e.target.textContent;
+    if (hiddenDisplay && inputNumber && lastOperation) {
+      mathOperation();
+    } else {
+      result = parseFloat(inputNumber);
+    }
+    clearVar(operationName);
+    lastOperation = operationName;
+  });
+});
+function clearVar(name = "") {
+  hiddenDisplay += inputNumber + " " + name + " ";
+  input.value = result;
+  inputNumber = "";
+  console.log(hiddenDisplay);
+}
+function mathOperation() {
+  if (lastOperation === "*") {
+    result = parseFloat(result) * parseFloat(inputNumber);
+  } else if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(inputNumber);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(inputNumber);
+  } else if (lastOperation === "/") {
+    result = parseFloat(result) / parseFloat(inputNumber);
+  }
+}
+equals.addEventListener("click", (e) => {
+  if (!hiddenDisplay || !inputNumber) return;
+  haveDot = false;
 });
