df8495ded1f4223184a550333f412b718f428162
Onyinye Chukwuma <onyinyechukwuma14@gmail.com>
2021-12-30 23:18:00 +0100

    'added-key-function'

--- index.js
+++ index.js
@@ -72,3 +72,42 @@ deleteLast.addEventListener("click", (e) => {
   inputNumber = inputNumber.slice(0, -1);
   input.value = inputNumber;
 });
+
+window.addEventListener("keydown", (e) => {
+  if (
+    e.key === "0" ||
+    e.key === "1" ||
+    e.key === "2" ||
+    e.key === "3" ||
+    e.key === "4" ||
+    e.key === "5" ||
+    e.key === "6" ||
+    e.key === "7" ||
+    e.key === "8" ||
+    e.key === "9" ||
+    e.key === "."
+  ) {
+    clickButton(e.key);
+  } else if (e.key === "*" || e.key === "+" || e.key === "-" || e.key === "/") {
+    clickOperation(e.key);
+  } else if (e.key === "Enter" || e.key === "=") {
+    clickEqual();
+  }
+});
+function clickButton(key) {
+  controls.forEach((button) => {
+    if (button.textContent === key) {
+      button.click();
+    }
+  });
+}
+function clickOperation(key) {
+  operations.forEach((button) => {
+    if (button.textContent === key) {
+      button.click();
+    }
+  });
+}
+function clickEqual() {
+  equals.click();
+}
